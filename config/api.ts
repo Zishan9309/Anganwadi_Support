921ee4750b214570efc0a9da770ad9711f197b25
Zishan9309 <zishankhan9284@gmail.com>
2025-12-08 17:45:26 +0530

    Add files via upload

---
+++ config/api.ts
@@ -0,0 +1,4 @@
+// config/api.ts
+export const API_URL = "http://10.0.2.2:5000/api";
+// export const API_URL = "http://localhost:5000/api"; // iOS Simulator
+// export const API_URL = "http://192.168.1.xxx:5000/api"; // Real Device (replace xxx with your PC’s IP)
